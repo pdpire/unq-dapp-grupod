8d0245a0c37e4f54b6c25a566e5f728df54c37ab
drjm <derojasjonathan@gmail.com>
2016-09-13 12:53:02 -0300

    test removeFriend en FriendManager (con pdpire)

---
+++ desapp-grupod-backend/src/test/java/testModel/FriendManagerTest.java
@@ -0,0 +1,37 @@
+package testModel;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+import org.junit.Test;
+
+import model.FriendManager;
+import model.User;
+
+public class FriendManagerTest {
+	
+	@Test
+	public void addFriendTest(){
+		FriendManager friendManager = new FriendManager();
+		User user = mock(User.class);
+		
+		friendManager.addFriend(user);
+		
+		assertEquals(friendManager.getUsers().size(),1);
+		assertEquals(friendManager.getUsers().get(0), user);
+	}
+	
+	
+	@Test
+	public void removeFriendTest(){
+		FriendManager friendManager = new FriendManager();
+		User user = mock(User.class);
+		friendManager.getUsers().add(user);
+		
+		friendManager.removeFriend(user);
+		
+		assertEquals(friendManager.getUsers().size(),0);
+	}
+
+
+}
