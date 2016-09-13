4145ffedd26ced748b4c17965e4aeb6ba5a2377f
drjm <derojasjonathan@gmail.com>
2016-09-13 11:56:53 -0300

    correcion junit en pom

--- desapp-grupod-backend/src/test/java/testModel/UserTest.java
+++ desapp-grupod-backend/src/test/java/testModel/UserTest.java
@@ -2,10 +2,8 @@ package testModel;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
 
-import junit.framework.TestCase;
 import model.FriendManager;
 import model.User;
 
@@ -24,19 +22,6 @@ public class UserTest{
 		
 		verify(friendManager, times(1)).addFriend(friend);
 	}
-	
-	@Test
-	public void removeFriendTest(){
-		FriendManager friendManager = mock(FriendManager.class);
-		User user = new User();
-		User friend = new User();
-		user.setFriends(friendManager);
-		
-		doNothing().when(friendManager).removeFriend(friend);
-		
-		user.removeFriend(friend);
-		
-		verify(friendManager, times(1)).removeFriend(friend);
-	}
+
 
 }
