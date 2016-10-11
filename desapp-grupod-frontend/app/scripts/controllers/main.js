3e056654132070d288d403593ea1b418ea765ca2
Pablo Pire <Pablo Pire>
2016-10-11 20:58:42 -0300

    comiteamos frontend, proyecto angular

---
+++ desapp-grupod-frontend/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name adondevamosgrupodApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the adondevamosgrupodApp
+ */
+angular.module('adondevamosgrupodApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
