082266d0016660c0d694c342cc2f5f1e67666fe0
Pablo Pire <Pablo Pire>
2016-11-29 11:54:47 -0300

    log con aspectos

--- desapp-grupod-backend/src/test/java/testArchitecture/ServiceTest.java
+++ desapp-grupod-backend/src/test/java/testArchitecture/ServiceTest.java
@@ -2,8 +2,10 @@ package testArchitecture;
 
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Method;
+import java.util.ArrayList;
 
 import org.junit.Test;
+import org.reflections.Reflections;
 
 import services.GenericService;
 
@@ -41,6 +43,38 @@ public class ServiceTest {
 		}
 
 		assertTrue(retFinal);
+	}
+	
+	
+	@Test
+	public void testPackegeService() throws InstantiationException, IllegalAccessException {
+
+		Integer errores = 0;
+
+		ArrayList<String> packages = new ArrayList<String>();
+
+		packages.add("model");
+		packages.add("persistence");
+		packages.add("webService");
+		packages.add("dto");
+		packages.add("builders");
+		//packages.add("service");
+
+		for (String pack : packages) {
+			
+			Object[] clases = this.getAllClassForPackage(pack, GenericService.class);
+			errores += clases.length;
+		}
+		
+		assertTrue(0 == errores);
+	}
+	
+	public Object[] getAllClassForPackage(String namePackage, Class clase) {
+
+		Reflections reflections = new Reflections(namePackage);
+		Object[] classes = (reflections.getSubTypesOf(clase)).toArray();
+		return classes;
+
 	}
 
 }
